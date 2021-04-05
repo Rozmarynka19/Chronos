c99c5d9097b3a0a761b0c78e22940941a5fd346c
Monika Rozmarynowska <morozmarynowska@gmail.com>
2021-04-05 20:52:08 +0200

    Add init UI project

---
+++ ChronosApp/app/src/main/java/com/example/chronosapp/ui/home/HomeViewModel.java
@@ -0,0 +1,20 @@
+package com.example.chronosapp.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment - our\n \"Hi Mark," +
+                " today you've got to do xyz so get down to the damn work, nygga\" part");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ ChronosApp/app/src/main/java/com/example/chronosapp/ui/list/ListViewModel.java
@@ -0,0 +1,19 @@
+package com.example.chronosapp.ui.list;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ListViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ListViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is list fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
