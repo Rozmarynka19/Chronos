8cfcb0715985097f6550cd3889f6489c3cdf264d
Monika Rozmarynowska <morozmarynowska@gmail.com>
2021-05-02 20:43:17 +0200

    Add scripts.

---
+++ skrypty/register.php
@@ -0,0 +1,29 @@
+<?php
+include 'databaseInfo.php';
+$conn = mysqli_connect($servername, $mysql_user, $mysql_password, $dbname);
+if($conn){
+	echo("connection sucess");
+}
+else{
+	echo("connection failed");
+}
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	if(isset($_POST['login'])){ $login = $_POST['login']; }
+	if(isset($_POST['password'])){ $pass = $_POST['password']; }
+	if(isset($_POST['email'])){ $email=$_POST['email']; }
+	if(isset($_POST['phone'])){ $phone=$_POST['phone']; }
+	$crypted = password_hash($pass, PASSWORD_DEFAULT);
+	$query="INSERT INTO user (Login, Password, Email, Phone) VALUES ('$login', '$crypted', '$email',  '$phone')";
+	if(mysqli_query($conn, $query)){
+		echo("\nregistered succesfully");
+		$userData = "\n".$login."\n".$email."\n".$phone;
+		echo($userData);
+	}
+	else{
+		echo("error in registration");
+	}	
+}
+else{
+	echo("error in request");
+}
+?>
