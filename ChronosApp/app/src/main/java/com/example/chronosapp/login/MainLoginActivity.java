72057886a1f2eef0fa6bd8ff7835f2aead3eddd6
mo44415 <mo44415@zut.edu.pl>
2021-05-02 22:28:07 +0200

    Update login, done form and show/hide password

--- ChronosApp/app/src/main/java/com/example/chronosapp/login/MainLoginActivity.java
+++ ChronosApp/app/src/main/java/com/example/chronosapp/login/MainLoginActivity.java
@@ -3,10 +3,15 @@ package com.example.chronosapp.login;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,11 +23,31 @@ public class MainLoginActivity extends AppCompatActivity implements View.OnClick
     private TextView register;
     private Button signButton;
     private EditText editTextLogin, editTextPassword;
+    private ImageView passwordShown;
 
+    private Boolean isPasswordShown = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_main);
+        setContentView(R.layout.login_layout);
+
+        passwordShown = (ImageView)findViewById(R.id.show_password_image);
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.show_password);
+
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isPasswordShown){
+                    passwordShown.setImageResource(R.drawable.login_eye);
+                    editTextPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }else{
+                    passwordShown.setImageResource(R.drawable.login_close_32);
+                    editTextPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                isPasswordShown = !isPasswordShown;
+            }
+        });
+
 
         @SuppressLint("WrongConstant")
         SharedPreferences sharedPreferences = getSharedPreferences("userDataSharedPref", MODE_APPEND);
