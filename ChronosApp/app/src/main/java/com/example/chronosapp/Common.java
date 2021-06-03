63126e494ebbaa13d3dea0c1a1712ddac1ab7c6b
Garin2137 <kk44386@zut.edu.pl>
2021-06-03 13:00:06 +0200

    adding 3 example list after registration

--- ChronosApp/app/src/main/java/com/example/chronosapp/Common.java
+++ ChronosApp/app/src/main/java/com/example/chronosapp/Common.java
@@ -6,7 +6,7 @@ public class Common {
 //                "http://192.168.56.2/chronos/"                 //localMrServer
 //                "http://de42e33b4c60.ngrok.io/chronos/"         //localMrServer
 //            "http://192.168.8.105/Example/"                        //localJkServer
-            "http://100.101.5.88/1213146_fsa523/"
+            "http://192.168.56.1/1213146_fsa523/" //localServerKK
     ;
     public static String getDbAddress() {return dbAddress;}
 }
---
+++ skrypty/register.php
@@ -0,0 +1,36 @@
+<?php
+include 'databaseInfo.php';
+$conn = mysqli_connect($servername, $mysql_user, $mysql_password, $dbname);
+if($conn){
+	echo("connection sucess");
+}
+else{
+	echo("connection failed");
+}
+if($_POST){
+	if(isset($_POST['login'])){ $login = $_POST['login']; }
+	if(isset($_POST['password'])){ $pass = $_POST['password']; }
+	if(isset($_POST['email'])){ $email=$_POST['email']; }
+	if(isset($_POST['phone'])){ $phone=$_POST['phone']; }
+	$crypted = password_hash($pass, PASSWORD_DEFAULT);
+	$query="INSERT INTO registered_users (User_Name, User_Password, User_Email, User_Phone) VALUES ('$login', '$crypted', '$email',  '$phone')";
+	if(mysqli_query($conn, $query)){
+		echo("\nregistered succesfully");
+		$query = "SELECT * FROM registered_users WHERE `User_Name`='" . $login . "'";
+		$result = $conn->query($query);
+		$user = $result->fetch_assoc();
+		$userData = "\n".$user['User_ID']."\n".$login."\n".$email."\n".$phone;
+		
+		$userID = $user['User_ID'];
+		$sql_new_lists="INSERT INTO user_lists(User_ID, List_Name) VALUES ($userID, 'Dom'),($userID, 'Praca'),($userID, 'Rachunki')";
+		$result_new_lists = $conn->query($sql_new_lists);
+		echo($userData);
+	}
+	else{
+		echo("error in registration");
+	}	
+}
+else{
+	echo("error in request");
+}
+?>
